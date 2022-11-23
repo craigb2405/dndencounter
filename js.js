ecfa0e51381f449e6278bc0c5988d17f6677b463
Craig Butler <craig.butler@jrbsolutions.co.uk>
2022-11-23 20:59:52 +0000

    created class constructor to build player and enemy variables, moved diceroll functions into methods, added health indicators for player and enenemies, re positioned dice roll box on html.

--- js.js
+++ js.js
@@ -1,53 +1,149 @@
-let rollButton = document.getElementById("playerRoll");
+class fighter {
+  constructor(health, attack, str, dex, con, wis, int, cha){
+    this.health = health
+    this.attack = attack
+    this.str = str
+    this.dex = dex
+    this.con = con
+    this.wis = wis
+    this.int = int
+    this.cha = cha
+
+  }
+
+  diceRollStr() {
+    let d20ResultPlayer = document.getElementById("strResultPlayer");
+    let d20ResultEnemy = document.getElementById("strResultEnemy");
+    let resultTextStr = document.getElementById("resultTextStr");
+    let d20player = Math.floor(Math.random() * 20 + 1);
+    let d20Enemy = Math.floor(Math.random() * 20 + 1);
+    let d20ResultPlayerAdj = d20player + player.str;
+    let d20ResultEnemyAdj = d20Enemy + enemy.str;
+    d20ResultPlayer.innerHTML = d20ResultPlayerAdj
+    d20ResultEnemy.innerHTML = d20ResultEnemyAdj
+    if (d20ResultPlayerAdj > d20ResultEnemyAdj){
+      resultTextStr.innerText = "player wins"
+      enemyHealthMinus()
+    } else resultTextStr.innerText = "enemy wins"
+     
+  }
+
+  diceRollDex() {
+    let d20ResultPlayer = document.getElementById("dexResultPlayer");
+    let d20ResultEnemy = document.getElementById("dexResultEnemy");
+    let resultTextDex = document.getElementById("resultTextDex");
+    let d20player = Math.floor(Math.random() * 20 + 1);
+    let d20Enemy = Math.floor(Math.random() * 20 + 1);
+    let d20ResultPlayerAdj = d20player + player.dex;
+    let d20ResultEnemyAdj = d20Enemy + enemy.dex;
+    d20ResultPlayer.innerHTML = d20ResultPlayerAdj
+    d20ResultEnemy.innerHTML = d20ResultEnemyAdj
+    if (d20ResultPlayerAdj > d20ResultEnemyAdj){
+      resultTextDex.innerText = "player wins"
+    } else resultTextDex.innerText = "enemy wins"
+   
+  }
+
+  diceRollCon() {
+    let d20ResultPlayer = document.getElementById("conResultPlayer");
+    let d20ResultEnemy = document.getElementById("conResultEnemy");
+    let resultTextCon = document.getElementById("resultTextCon");
+    let d20player = Math.floor(Math.random() * 20 + 1);
+    let d20Enemy = Math.floor(Math.random() * 20 + 1);
+    let d20ResultPlayerAdj = d20player + player.str;
+    let d20ResultEnemyAdj = d20Enemy + enemy.str;
+    d20ResultPlayer.innerHTML = d20ResultPlayerAdj
+    d20ResultEnemy.innerHTML = d20ResultEnemyAdj
+    if (d20ResultPlayerAdj > d20ResultEnemyAdj){
+      resultTextCon.innerText = "player wins"
+    } else resultTextCon.innerText = "enemy wins"
+   
+  }
+
+  diceRollWis() {
+    let d20ResultPlayer = document.getElementById("wisResultPlayer");
+    let d20ResultEnemy = document.getElementById("wisResultEnemy");
+    let resultTextWis = document.getElementById("resultTextWis");
+    let d20player = Math.floor(Math.random() * 20 + 1);
+    let d20Enemy = Math.floor(Math.random() * 20 + 1);
+    let d20ResultPlayerAdj = d20player + player.str;
+    let d20ResultEnemyAdj = d20Enemy + enemy.str;
+    d20ResultPlayer.innerHTML = d20ResultPlayerAdj
+    d20ResultEnemy.innerHTML = d20ResultEnemyAdj
+    if (d20ResultPlayerAdj > d20ResultEnemyAdj){
+      resultTextWis.innerText = "player wins"
+    } else resultTextWis.innerText = "enemy wins"
+   
+  }
+
+  diceRollInt() {
+    let d20ResultPlayer = document.getElementById("intResultPlayer");
+    let d20ResultEnemy = document.getElementById("intResultEnemy");
+    let resultTextInt = document.getElementById("resultTextInt");
+    let d20player = Math.floor(Math.random() * 20 + 1);
+    let d20Enemy = Math.floor(Math.random() * 20 + 1);
+    let d20ResultPlayerAdj = d20player + player.str;
+    let d20ResultEnemyAdj = d20Enemy + enemy.str;
+    d20ResultPlayer.innerHTML = d20ResultPlayerAdj
+    d20ResultEnemy.innerHTML = d20ResultEnemyAdj
+    if (d20ResultPlayerAdj > d20ResultEnemyAdj){
+      resultTextInt.innerText = "player wins"
+    } else resultTextInt.innerText = "enemy wins"
+   
+  }
+
+  diceRollCha() {
+    let d20ResultPlayer = document.getElementById("chaResultPlayer");
+    let d20ResultEnemy = document.getElementById("chaResultEnemy");
+    let resultTextCha = document.getElementById("resultTextCha");
+    let d20player = Math.floor(Math.random() * 20 + 1);
+    let d20Enemy = Math.floor(Math.random() * 20 + 1);
+    let d20ResultPlayerAdj = d20player + player.str;
+    let d20ResultEnemyAdj = d20Enemy + enemy.str;
+    d20ResultPlayer.innerHTML = d20ResultPlayerAdj
+    d20ResultEnemy.innerHTML = d20ResultEnemyAdj
+    if (d20ResultPlayerAdj > d20ResultEnemyAdj){
+      resultTextCha.innerText = "player wins"
+    } else resultTextCha.innerText = "enemy wins"
+   
+  }
+
+  
+}
+
+let player = new fighter(100, 5, +2, +1, 0, -1, +1, 0)
+let enemy = new fighter(100, 5, +2, +1, 0, -1, +1, 0)
+
+
+
 let textBox = document.getElementById("text");
-let str = document.getElementById("str").innerText;
-let dex = document.getElementById("dex").innerText;
-let con = document.getElementById("con").innerText;
-let wis = document.getElementById("wis").innerText;
-let int = document.getElementById("int").innerText;
-let cha = document.getElementById("cha").innerText;
 
-//Enemy
-let enemyHealth = 200;
-let enemyAttack = 10;
+let strength = document.getElementById('str')
+strength.innerHTML = player.str
 
-//Hero
-let heroHealth = 200;
-let heroAttack = 10;
+let dexterity = document.getElementById('dex')
+dexterity.innerHTML = player.dex
 
-//create arugment which takes the stat
-function diceRollStr() {
-  var d20Result = document.getElementById("strResult");
-  var d20 = Math.floor(Math.random() * 20 + 1);
-  d20Result.innerHTML = d20 + str;
-}
+let constitution = document.getElementById('con')
+con.innerHTML = player.con
 
-function diceRollDex() {
-  var d20Result = document.getElementById("dexResult");
-  var d20 = Math.floor(Math.random() * 20 + 1);
-  dexResult.innerHTML = d20 + dex;
-}
-function diceRollCon() {
-  var d20Result = document.getElementById("conResult");
-  var d20 = Math.floor(Math.random() * 20 + 1);
-  conResult.innerHTML = d20 + con;
-}
-function diceRollWis() {
-  var d20Result = document.getElementById("wisResult");
-  var d20 = Math.floor(Math.random() * 20 + 1);
-  wisResult.innerHTML = d20 + wis;
-}
-function diceRollInt() {
-  var d20Result = document.getElementById("intResult");
-  var d20 = Math.floor(Math.random() * 20 + 1);
-  intResult.innerHTML = d20 + int;
-}
-function diceRollCha() {
-  var d20Result = document.getElementById("chaResult");
-  var d20 = Math.floor(Math.random() * 20 + 1);
-  chaResult.innerHTML = d20 + cha;
+let wisdom = document.getElementById('wis')
+wis.innerHTML = player.wis
+
+let intelligence = document.getElementById('int')
+int.innerHTML = player.int
+
+let charisma = document.getElementById('cha')
+cha.innerHTML = player.cha
+
+let playerHealth = document.getElementById('playerHealth')
+let enemyHealth = document.getElementById('enemyHealth')
+playerHealth.innerHTML = player.health
+enemyHealth.innerHTML = enemy.health
+
+function enemyHealthMinus (){
+  enemy.health -= player.attack
 }
 
-//enemy has stats randomly generated
-//enemy rolls dice
-//
+
+
